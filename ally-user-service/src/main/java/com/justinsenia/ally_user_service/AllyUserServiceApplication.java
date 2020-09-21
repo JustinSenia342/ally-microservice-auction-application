fe9f17b7f0f40cd56ad0a3221152fb62ab8d3495
Justin Senia <justinsenia342@gmail.com>
2020-09-21 01:52:25 -0400

    'MYSQL SERVICE FUNCTIONAL
    User service succesfully integrates
    with local MySQL DB.'

---
+++ ally-user-service/src/main/java/com/justinsenia/ally_user_service/AllyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.justinsenia.ally_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllyUserServiceApplication.class, args);
+	}
+
+}
---
+++ ally-user-service/src/test/java/com/justinsenia/ally_auction_service/AllyUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinsenia.ally_auction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllyUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
