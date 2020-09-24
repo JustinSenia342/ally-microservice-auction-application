58d1a8f7c928173339d7c7aaf0e0f5b2b5190e4f
Justin Senia <justinsenia342@gmail.com>
2020-09-24 04:41:56 -0400

    NAMING SERVER FUNCTIONAL

---
+++ ally-eureka-naming-server/src/test/java/com/justinsenia/ally_eureka_naming_server/AllyEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinsenia.ally_eureka_naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllyEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
