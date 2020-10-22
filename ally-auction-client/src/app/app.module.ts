3505bee464e001265ff85a3a3c4bbe6d5b3fee74
Justin Senia <justinsenia342@gmail.com>
2020-10-21 21:21:51 -0400

    ANGULAR OKTA INTEGRATION
    Integrated Okta with Angular
    frontend and springboot backend.

--- ally-auction-client/src/app/app.module.ts
+++ ally-auction-client/src/app/app.module.ts
@@ -2,12 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CreateAuctionComponent } from './create-auction/create-auction.component';
 import { AuctionDetailsComponent } from './auction-details/auction-details.component';
 import { AuctionListComponent } from './auction-list/auction-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UpdateAuctionComponent } from './update-auction/update-auction.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { ProtectedComponent } from './protected/protected.component';
+import { LoginComponent } from './login/login.component';
+import {
+	OKTA_CONFIG,
+	OktaAuthModule,
+	OktaCallbackComponent,
+	OktaAuthGuard
+} from '@okta/okta-angular';
+
+const config = {
+  issuer: 'https://dev-5184859.okta.com/oauth2/default',
+  redirectUri: window.location.origin + '/login/callback',
+  clientId: '0oac4kj5oa5WzIz915d5',
+  pkce: true
+}
+
+export function onAuthRequired(oktaAuth, injector) {
+  const router = injector.get(Router);
+
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'login/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ OktaAuthGuard ],
+    data: {
+      onAuthRequired
+    }
+  }
+]
 
 @NgModule({
   declarations: [
@@ -15,15 +58,22 @@ import { UpdateAuctionComponent } from './update-auction/update-auction.componen
     CreateAuctionComponent,
     AuctionDetailsComponent,
     AuctionListComponent,
-    UpdateAuctionComponent
+    UpdateAuctionComponent,
+    ProtectedComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+	RouterModule.forRoot(appRoutes),
+	OktaAuthModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthRoutingModule
   ],
-  providers: [],
+  providers: [
+  { provide: OKTA_CONFIG, useValue: config },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
