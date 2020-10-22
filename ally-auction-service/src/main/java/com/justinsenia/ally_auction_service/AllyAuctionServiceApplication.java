b3857ebac36a178998c66a9df69c397103df28a5
Justin Senia <justinsenia342@gmail.com>
2020-10-22 18:48:23 -0400

    ADDED COMMENTS TO JAVA FILES

--- ally-auction-service/src/main/java/com/justinsenia/ally_auction_service/AllyAuctionServiceApplication.java
+++ ally-auction-service/src/main/java/com/justinsenia/ally_auction_service/AllyAuctionServiceApplication.java
@@ -21,18 +21,19 @@ public class AllyAuctionServiceApplication {
 	}
 
 	
-	 @Bean
-	    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        CorsConfiguration config = new CorsConfiguration();
-	        config.setAllowCredentials(true);
-	        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-	        config.setAllowedMethods(Collections.singletonList("*"));
-	        config.setAllowedHeaders(Collections.singletonList("*"));
-	        source.registerCorsConfiguration("/**", config);
-	        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-	        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	        return bean;
-	    }
+	// Bean that handles CORS configuration
+	@Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 	
 }
